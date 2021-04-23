1bde352e6a7b7097d9e4b2a0a886fffae01fce89
skill347 <tymszczur@gmail.com>
2021-04-23 16:06:23 +0200

    Added inventory toggling with "TAB"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,15 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private Transform target;
+    [SerializeField] private bool eqVisible;
+    [SerializeField] private GameObject eq;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        eqVisible = false;
+        eq.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,5 +24,15 @@ public class PlayerController : MonoBehaviour
         {
             target.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+        if(eqVisible && Input.GetButtonDown("Inventory"))
+        {
+            eqVisible = false;
+            eq.SetActive(false);
+        } else if(!eqVisible && Input.GetButtonDown("Inventory"))
+        {
+            eqVisible = true;
+            eq.SetActive(true);
+        }
+
     }
 }
